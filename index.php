da0bb56a68acc2e969b7d8ec7815a500ef5f0e73
Indraj Bahra <indraj.bahra@googlemail.com>
2019-07-10 21:07:44 +0100

    added contact php

--- index.php
+++ index.php
@@ -1,3 +1,67 @@
+<?php
+
+    $error = ""; $successMessage = "";
+
+    if ($_POST) {
+
+        if (!$_POST["email"]) {
+
+            $error .= "An email address is required.<br>";
+
+        }
+
+        if (!$_POST["content"]) {
+
+            $error .= "The content field is required.<br>";
+
+        }
+
+        if (!$_POST["subject"]) {
+
+            $error .= "The subject is required.<br>";
+
+        }
+
+        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+
+            $error .= "The email address is invalid.<br>";
+
+        }
+
+        if ($error != "") {
+
+            $error = '<div class="alert alert-danger" role="alert"><p>There were error(s) in your form:</p>' . $error . '</div>';
+
+        } else {
+
+            $emailTo = "me@mydomain.com";
+
+            $subject = $_POST['subject'];
+
+            $content = $_POST['content'];
+
+            $headers = "From: ".$_POST['email'];
+
+            if (mail($emailTo, $subject, $content, $headers)) {
+
+                $successMessage = '<div class="alert alert-success" role="alert">Your message was sent, we\'ll get back to you ASAP!</div>';
+
+
+            } else {
+
+                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later</div>';
+
+
+            }
+
+        }
+
+
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -13,26 +77,26 @@
 
       <div class="container">
 
-        <h1>Get in touch!</h1>
+    <h1>Get in touch!</h1>
 
-          <div id="error"></div>
+      <div id="error"><? echo $error.$successMessage; ?></div>
 
-          <form method="post">
-            <fieldset class="form-group">
-              <label for="email">Email address</label>
-              <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
-              <small class="text-muted">We'll never share your email with anyone else.</small>
-            </fieldset>
-            <fieldset class="form-group">
-              <label for="subject">Subject</label>
-              <input type="text" class="form-control" id="subject" name="subject" >
-            </fieldset>
-            <fieldset class="form-group">
-              <label for="exampleTextarea">What would you like to ask us?</label>
-              <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-            </fieldset>
-            <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-          </form>
+      <form method="post">
+  <fieldset class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+    <small class="text-muted">We'll never share your email with anyone else.</small>
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" id="subject" name="subject" >
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="exampleTextarea">What would you like to ask us?</label>
+    <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+  </fieldset>
+  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+</form>
 
         </div>
 
